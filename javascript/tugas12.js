a37526ea2289bf6e211567624dc99b972d92be09
gilang_gans <gilangsos99@gmail.com>
2022-05-15 17:39:52 +0700

    Tugas js 12
    Tugas ini untuk niomic

---
+++ javascript/tugas12.js
@@ -0,0 +1,15 @@
+function array(){
+  var buah = ['Pisang','Jeruk','Apel','Mangga']
+  var buah2 = ['Jeruk','Apel','Mangga']
+  console.log(buah)
+  console.log(buah2)
+  buah.pop();
+  buah.shift();
+  buah2.pop();
+  buah2.shift();
+  return buah
+  return buah2
+
+}
+
+console.log(array());
